b3ab524848e2ebf36992d2fdeaf8067b46729005
Alex <perematom@gmail.com>
2024-11-23 12:02:19 +0100

    Improving instalation

--- fix_admin_filter.py
+++ fix_admin_filter.py
@@ -1,10 +1,40 @@
+import subprocess
+import sys
 from pathlib import Path
 from os import path
 
-
 BASE_DIR = Path(__file__).resolve().parent
 
-DJANGO_ADMIN_FILTER_PATH_FILE = path.join(BASE_DIR, "venv\\Lib\\site-packages\\django_admin_filter\\urls.py")
+commands_instalation = [
+	"python -m pip install upgrade pip",
+	"pip install wheel",
+	"pip install setuptools",
+	"pip install -r requirements.txt"
+]
+
+commands_migrations = [
+	"python manage.py makemigrations",
+	"python manage.py makemigrations mainapp",
+	"python manage.py makemigrations forum",
+	"python manage.py makemigrations authentication",
+	"python manage.py migrate"
+]
+
+virtual_env_path = path.join(BASE_DIR, "venv", "Scripts", "activate")
+
+exec(open(virtual_env_path).read(), dict(__file__=virtual_env_path))
+
+def commands_run():
+	result = subprocess.run(commands_instalation, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	print(result.stdout.decode())
+	print(result.stderr.decode())
+
+
+for com in commands_instalation:
+	commands_run(com)
+
+
+DJANGO_ADMIN_FILTER_PATH_FILE = path.join(BASE_DIR, "venv","Lib","site-packages","django_admin_filter","urls.py")
 
 check_path = Path(DJANGO_ADMIN_FILTER_PATH_FILE)
 
@@ -30,3 +60,7 @@ else:
 
 with open(DJANGO_ADMIN_FILTER_PATH_FILE, 'w') as file:
 	file.writelines(lines)
+
+
+for com in commands_migrations:
+	commands_run(com)
